d6e2ec6728ef7579f54deeb71b7a31364be6c7b6
jojooah <my_sg1@naver.com>
2021-11-09 15:22:08 +0900

    apply the Spring Security

---
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.shop.config;
+
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MemberService memberService;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.formLogin()
+                .loginPage("/members/login")//로그인 페이지 url설정
+                .defaultSuccessUrl("/") //로그인 성공시 이동할 url설정
+                .usernameParameter("email")//로그인 시 사용할 파라미터 이름으로 email지정
+                .failureUrl("/members/login/error") //로그인 실패 시 이동할 url설정
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher
+                        ("/members/logout"))//로그아웃 url설정
+                .logoutSuccessUrl("/")//로그아웃 성공시 이동할 url설정
+                ;
+        http.authorizeRequests()
+                .mvcMatchers("/","/members/**","/item**","/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(memberService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/css/**","/js/**","/img/**");
+    }
+}
