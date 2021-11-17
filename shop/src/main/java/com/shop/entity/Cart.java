5bcb7d674e687ce9a5024493b63ecca44557d94e
jojooah <my_sg1@naver.com>
2021-11-18 04:49:38 +0900

    cart

--- shop/src/main/java/com/shop/entity/Cart.java
+++ shop/src/main/java/com/shop/entity/Cart.java
@@ -20,6 +20,11 @@ public class Cart{
     private Member member;
 
 
+    public static Cart createCart(Member member){
+        Cart cart=new Cart();
+        cart.setMember(member);
+        return cart;
+    }
 
 
 }
